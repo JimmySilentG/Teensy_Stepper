12a81af54caa6e8321832b05344655c2658bdeec
JimmySilentG <jimmygnesda@gmail.com>
2024-02-09 18:08:00 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <AccelStepper.h>
+
+// put function declarations here:
+int EnablePin = 2;
+int DirPin = 3;
+int PulPin = 4;
+
+AccelStepper stepper(1, PulPin, DirPin);
+
+void setup() {
+  pinMode(EnablePin, OUTPUT);
+  digitalWrite(EnablePin, LOW);
+  stepper.setMaxSpeed(30); //6400 should be 8 revolutions per second which is max start speed for motor
+  stepper.setAcceleration(10);
+  stepper.moveTo(1400);
+}
+
+void loop() {
+  stepper.run();
+  if (abs(stepper.distanceToGo()) <= 10){
+    stepper.moveTo(0);
+   } 
+}
