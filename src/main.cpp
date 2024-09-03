8035e99df2a485579eedc24a88174252391308d4
JimmySilentG <jimmygnesda@gmail.com>
2024-09-03 18:30:11 -0400

    Globoid

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ AccelStepper stepper(1, PulPin, DirPin);
 void setup() {
   pinMode(EnablePin, OUTPUT);
   digitalWrite(EnablePin, LOW);
-  stepper.setMaxSpeed(30); //6400 should be 8 revolutions per second which is max start speed for motor
-  stepper.setAcceleration(10);
-  stepper.moveTo(1450);
+  stepper.setMaxSpeed(8000); //6400 should be 8 revolutions per second which is max start speed for motor
+  stepper.setAcceleration(1500);
+  stepper.moveTo(250000);
 }
 
 void loop() {
